fa68b1fd2b8b9a213eb35865b4725b7fdec4fdb7
Cristian omar <Cristian omar@DESKTOP-ASS6B4I>
2023-01-01 18:44:28 -0600

    Programa para saber las estaciones del año, en caso de no existir la estación el programa muestra un mensaje

---
+++ src/openproyectodos/OpenProyectoDos.java
@@ -0,0 +1,28 @@
+
+package openproyectodos;
+
+
+public class OpenProyectoDos {
+
+    public static void main(String[] args) {
+        
+        String estacion = "invierno";
+        
+        if(estacion =="primavera"){
+            System.out.println("Es Primavera");
+        }
+        else if(estacion =="verano"){
+               System.out.println("es Verano");
+        }  
+        else if(estacion =="otoño"){
+               System.out.println("es Otoño");
+        }
+        else if(estacion =="invierno"){
+               System.out.println("es Invierno");
+        }
+        else{
+            System.out.println("La Estación no existe");
+        }
+    }
+    
+}
